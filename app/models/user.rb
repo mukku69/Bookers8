81d437ad7bbed0529734df06829a117c8b1c22e8
mukku69 <programing0803@gmail.com>
2021-05-03 05:26:13 +0000

    検索機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,34 @@ class User < ApplicationRecord
 
   # ====================自分がフォローしているユーザーとの関連 ===================================
   #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
   has_many :followeds, through: :active_relationships, source: :follower
   # ========================================================================================
 
   # ====================自分がフォローされるユーザーとの関連 ===================================
   #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
   has_many :followers, through: :passive_relationships, source: :followed
   # =======================================================================================
 
   def followed_by?(user)
     passive_relationships.find_by(followed_id: user.id).present?
+  end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
   end
 
   attachment :profile_image, destroy: false
