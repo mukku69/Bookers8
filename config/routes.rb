ee56eeda5e689e377cf3617f0fa14d87932cf35f
mukku69 <programing0803@gmail.com>
2021-05-05 08:45:52 +0000

    メール送信機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: "users/registrations"
+}
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
   get "search" => "searches#search"
-  
+
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
